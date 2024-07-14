b6b89a27c8fd9bf0cba83dd3967f1c90e70c9ba9
kadirosmanust <kadirosmanust98@gmail.com>
2024-07-14 13:54:56 +0300

    feat: create stores

---
+++ src/store.js
@@ -0,0 +1,13 @@
+const initialState = {
+  basket: [],
+  total: 0,
+};
+
+const state = { ...initialState };
+
+export const getState = () => state;
+
+export const setState = (key, value) => {
+  if (state[key] === undefined) return;
+  state[key] = value;
+};
