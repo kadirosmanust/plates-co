3c6f25430f5f12f355a07dc4133e8af94cd0cd3a
kadirosmanust <kadirosmanust98@gmail.com>
2024-07-14 23:03:22 +0300

    feat: add components index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import '../public/styles.css';
 import Campaigns from './components/Campaigns';
+import DeliveryFeeInfo from './components/DeliveryFeeInfo';
 import Header from './components/Header';
+import ProductList from './components/ProductList';
 
 document.addEventListener('DOMContentLoaded', () => {
   Header();
   Campaigns();
+  DeliveryFeeInfo();
+  ProductList();
 });
